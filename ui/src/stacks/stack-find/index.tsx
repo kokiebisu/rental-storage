969995ad42d303e87c526939bd71e11eb746c1a8
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-09-13 16:21:50 -0700

    As a user I want to display the map based on the search city (#8)

--- ui/src/stacks/stack-find/index.tsx
+++ ui/src/stacks/stack-find/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { FindHomeScreen } from "./home";
 import { stacksConfig } from "../../config";
+import { FindMapScreen } from "./map";
+import { FindHomeScreen } from "./home";
 
 const FindStack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export const FindStackScreen = () => {
   return (
     <FindStack.Navigator screenOptions={stacksConfig}>
       <FindStack.Screen name="Home" component={FindHomeScreen} />
+      <FindStack.Screen name="Map" component={FindMapScreen} />
     </FindStack.Navigator>
   );
 };
