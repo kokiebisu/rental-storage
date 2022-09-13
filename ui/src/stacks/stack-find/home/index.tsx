5ceb23dca5bfc467d7c7636860d2ebb1b04842d9
kokoaj <kokiebisu@icloud.com>
2022-09-13 08:50:04 -0700

    enable displaying a map full screen

--- ui/src/stacks/stack-find/home/index.tsx
+++ ui/src/stacks/stack-find/home/index.tsx
@@ -1,10 +1,26 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export const FindHomeScreen = () => {
   return (
     <View>
-      <Text>Find Home Screen</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
