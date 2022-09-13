bf4c47616fe1ee9a0040d2a78a969946ef79e012
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-09-13 08:57:35 -0700

    Enable displaying a map for the StorageScreen (#6)

--- ui/src/stacks/stack-my-storage/home/index.tsx
+++ ui/src/stacks/stack-my-storage/home/index.tsx
@@ -1,10 +1,26 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export const MyStorageHomeScreen = () => {
   return (
     <View>
-      <Text>My storage HOme Screen</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
