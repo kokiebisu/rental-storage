a72c11a1e8404e008782c7729cf20e7b12966cf6
kokoaj <kokiebisu@icloud.com>
2022-09-23 12:14:41 -0700

    enabled bookingCreate event sqs-sns

--- packages/booking/src/publisher.ts
+++ packages/booking/src/publisher.ts
@@ -5,13 +5,24 @@ import {
 } from "@rental-storage-project/common";
 
 export class BookingPublisherService extends PublisherService {
-  public async create(region: AWSRegion): Promise<BookingPublisherService> {
-    return new BookingPublisherService(region);
+  public static async create(
+    region: AWSRegion
+  ): Promise<BookingPublisherService> {
+    if (!process.env.account_id) {
+      throw new Error("account_id was not successfully retrieved");
+    }
+    return new BookingPublisherService(region, process.env.account_id);
   }
 
   public async bookingCreated(booking: BookingInterface) {
     const topicArn = `${process.env.namespace}-${process.env.service}-${process.env.stage}-bookingCreated`;
-    const message = JSON.parse(booking);
-    await this.publish(message, topicArn);
+    console.debug("TOPIC ARN: ", topicArn);
+    const message = JSON.stringify(booking);
+    try {
+      await this.publish(message, topicArn);
+      console.log("Successfully published message");
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
