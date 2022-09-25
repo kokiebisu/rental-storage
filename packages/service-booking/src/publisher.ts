f70341b5b52a7bbc7200d897c62dbc16522b3100
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-09-24 18:07:45 -0700

    54 As a Developer, I want to set up a REST API for Booking Service (#55)

--- packages/service-booking/src/publisher.ts
+++ packages/service-booking/src/publisher.ts
@@ -8,10 +8,10 @@ export class BookingPublisherService extends PublisherService {
   public static async create(
     region: AWSRegion
   ): Promise<BookingPublisherService> {
-    if (!process.env.account_id) {
-      throw new Error("account_id was not successfully retrieved");
+    if (!process.env.accountId) {
+      throw new Error("accountId was not successfully retrieved");
     }
-    return new BookingPublisherService(region, process.env.account_id);
+    return new BookingPublisherService(region, process.env.accountId);
   }
 
   public async bookingCreated(booking: BookingInterface) {
