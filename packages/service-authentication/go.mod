b19abd5a85aae57941da80d4c133c576a16e0ca1
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-10-02 12:08:27 -0700

    Cleanup (#84)

---
+++ packages/service-authentication/go.mod
@@ -0,0 +1,7 @@
+module github.com/kokiebisu/rental-storage-project/service-authentication
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.34.1
+
+require golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,10 @@
-(cd packages/service-authorizer && make prepare)
-(cd packages/service-booking && npm install); 
-(cd packages/service-image && npm install); 
-(cd packages/service-listing && npm install); 
-(cd packages/service-user && npm install);
-(cd packages/service-item && npm install);
+# Go
+(cd packages/service-authorizer && make prepare || { echo 'failed' ; exit 1; });
+(cd packages/service-authentication && make prepare || { echo 'failed' ; exit 1; });
+
+# Typescript
+(cd packages/service-booking && npm install || { echo 'failed' ; exit 1; }); 
+(cd packages/service-image && npm install || { echo 'failed' ; exit 1; }); 
+(cd packages/service-listing && npm install || { echo 'failed' ; exit 1; }); 
+(cd packages/service-user && npm install || { echo 'failed' ; exit 1; });
+(cd packages/service-item && npm install || { echo 'failed' ; exit 1; });
