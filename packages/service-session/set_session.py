a9a2311cc019f44dc84ccd7538028f96914f3c76
kokoaj <kokiebisu@icloud.com>
2022-10-09 07:40:08 -0700

    cleanup

--- packages/service-session/set_session.py
+++
@@ -1,7 +0,0 @@
-import logging
-import service
-
-def handler(event, _):
-    logging.info("EVENT: ", event)
-    response = service.create_presigned_post('rental-storage-listing-dev-profile', 'random')
-    return response
