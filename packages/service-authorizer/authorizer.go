b918f7824ab1c6bb319d7522d5ba6d6cc306e624
kokoaj <kokiebisu@icloud.com>
2022-10-09 07:46:18 -0700

    updated code

--- packages/service-authorizer/authorizer.go
+++ packages/service-authorizer/authorizer.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,17 +16,42 @@ type Event struct {
 	RequestContext     events.AppSyncLambdaAuthorizerRequestContext `json:"requestContext"`
 }
 
+type JWTPayload struct {
+	UserId string `json:"userId"`
+}
+
 func HandleRequest(ctx context.Context, event Event) (*events.AppSyncLambdaAuthorizerResponse, error) {
-		token := event.AuthorizationToken
-		// check if token exists in redis session (rest api)
-		if token == "Authorized" {
-			return &events.AppSyncLambdaAuthorizerResponse{
-				IsAuthorized: true,
-			}, nil
+		jwt := event.AuthorizationToken
+		authenticationEndpoint := fmt.Sprintf("%s/auth/verify?auth=%s", os.Getenv("SERVICE_ENDPOINT"), jwt)
+		fmt.Println("ENDPOINT", authenticationEndpoint)
+		// // send REST API to verify jwt
+		resp, err := http.Get(authenticationEndpoint)
+		if err != nil {
+			return generateUnauthorizedResponse(), nil
 		}
-        return &events.AppSyncLambdaAuthorizerResponse{
-			IsAuthorized: false,
-		}, nil
+		fmt.Println("RESP", resp)
+		payload := JWTPayload{}
+		if err = json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+			return generateUnauthorizedResponse(), nil
+		}
+		fmt.Println("PAYLOAD", payload)
+
+		return generateAuthorizedResponse(payload.UserId), nil
+}
+
+func generateUnauthorizedResponse() *events.AppSyncLambdaAuthorizerResponse {
+	return &events.AppSyncLambdaAuthorizerResponse{
+		IsAuthorized: false,
+	}
+}
+
+func generateAuthorizedResponse(userId string) *events.AppSyncLambdaAuthorizerResponse {
+	return &events.AppSyncLambdaAuthorizerResponse{
+		IsAuthorized: true,
+		ResolverContext: map[string]interface{}{
+			userId: userId,
+		},
+	}
 }
 
 func main() {
