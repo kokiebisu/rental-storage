23de4bb9c66fbf9d7f30fa13fa41040ac4bac46c
kokoaj <kokiebisu@icloud.com>
2022-10-16 09:42:04 -0700

    add code

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,71 @@
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
+<<<<<<< Updated upstream
 
 import { MyTabs } from "./src/navigation/tab";
 import { ApolloProvider } from "@apollo/client";
 import { appsyncClient } from "./src/integration/graphql";
 
 import { ContextProvider } from "./src/context";
+=======
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ApolloProvider } from "@apollo/client";
+
+import { appsyncClient } from "./src/integration/graphql";
+import { Tabs } from "./src/navigation/tab";
+import { AuthContext } from "./src/context/auth";
+import SplashScreen from "./src/stacks/screen-splash";
+import { AuthSignInScreen } from "./src/stacks/stack-auth/signin";
+import { ContextProvider } from "./src/context";
+
+const Stack = createNativeStackNavigator();
+>>>>>>> Stashed changes
 
 export default () => {
   return (
     <ApolloProvider client={appsyncClient}>
       <ContextProvider>
+<<<<<<< Updated upstream
         <NavigationContainer>
           <MyTabs />
         </NavigationContainer>
         <StatusBar />
       </ContextProvider>
+=======
+        <Main />
+      </ContextProvider>
+      <StatusBar />
+>>>>>>> Stashed changes
     </ApolloProvider>
+  );
+};
+
+const Main = () => {
+  const { authState } = React.useContext(AuthContext);
+  const isSignedIn = authState.userToken == null;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <React.Fragment>
+          {isSignedIn ? (
+            <React.Fragment>
+              <Stack.Screen name="SignIn" component={AuthSignInScreen} />
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Stack.Screen
+                name="Tabs"
+                component={Tabs}
+                options={{ headerShown: false }}
+              />
+            </React.Fragment>
+          )}
+          <Stack.Group>
+            <Stack.Screen name="Splash" component={SplashScreen} />
+          </Stack.Group>
+        </React.Fragment>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
