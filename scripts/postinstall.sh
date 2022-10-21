b01ebc34a4564091865ce4cd3a6143c45b23dae4
kokoaj <kokiebisu@icloud.com>
2022-10-20 22:07:19 -0700

    test

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 # Go
 # (cd packages/service-authorizer && make prepare || { echo 'failed' ; exit 1; });
 # (cd packages/service-authentication && make prepare || { echo 'failed' ; exit 1; });
