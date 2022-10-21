549c7e28851c6986481b51c230224f9ff783372d
kokoaj <kokiebisu@icloud.com>
2022-10-20 21:09:52 -0700

    draft

--- packages/service-booking/test/service/makeBooking.test.ts
+++
@@ -1,9 +0,0 @@
-import { Currency } from "@rental-storage-project/common";
-import { BookingServiceImpl } from "../../src/service";
-
-describe("makeBooking()", () => {
-  it("shoud work", async () => {
-    const service = await BookingServiceImpl.create();
-    const data = await service.makeBooking(50, Currency.CAD, "1", "1");
-  });
-});
--- packages/service-booking/test/sqs.test.ts
+++
@@ -1,9 +0,0 @@
-import { AWSRegion } from "@rental-storage-project/common";
-import { BookingPublisherService } from "../src/publisher";
-
-describe.skip("findById()", () => {
-  it("shoud work", async () => {
-    const publisher = await BookingPublisherService.create(AWSRegion.US_EAST_1);
-    await publisher.bookingComplete();
-  });
-});
