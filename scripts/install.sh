4e9bc9ca75b62fbfa2a72179ba61a2c8446f59ca
kokoaj <kokiebisu@icloud.com>
2022-10-22 11:23:26 -0700

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,28 +1,34 @@
 #! /bin/sh
-echo "Setting up package root..."
+echo "Installing package root dependencies..."
 npm ci
 
-echo "Setting up Booking Service..."
+# typescript
+echo "Installing Booking Service dependencies..."
 cd packages/service-booking && npm ci && cd ../..
-echo "Setting up Image Service..."
-cd packages/service-image && npm ci && cd ../..
-echo "Setting up Listing Service..."
+echo "Installing Listing Service dependencies..."
 cd packages/service-listing && npm ci && cd ../..
-echo "Setting up User Service..."
+echo "Installing User Service dependencies..."
 cd packages/service-user && npm ci && cd ../..
 
-echo "Setting up Infrastructure..."
+# python
+echo "Installing Image Service dependencies..."
+cd packages/service-image && npm ci && cd ../..
+echo "Installing Slack Service dependencies..."
+cd packages/service-slack && npm ci && cd ../..
+
+# golang
+echo "Installing Authentication Service dependencies..."
+cd packages/service-authentication && npm ci && cd ../..
+echo "Installing Authorizer Service dependencies..."
+cd packages/service-authorizer && npm ci && cd ../..
+echo "Installing Payment Service dependencies..."
+cd packages/service-payment && npm ci && cd ../..
+
+echo "Installing Infrastructure dependencies..."
 cd infrastructure && npm ci && cd ..
 
-echo "Setting up Appsync..."
+echo "Installing Appsync dependencies..."
 cd api/appsync && npm ci && cd ../..
-echo "Setting up Api Gateway..."
+echo "Installing Api Gateway dependencies..."
 cd api/apigateway && npm ci && cd ../..
 
-# Go
-echo "Setting up Authorizer Service..."
-cd packages/service-authorizer && npm ci && cd ../..
-echo "Setting up Authentication Service..."
-cd packages/service-authentication && npm ci && cd ../..
-echo "Setting up Payment Service..."
-cd packages/service-payment && npm ci && cd ../..
