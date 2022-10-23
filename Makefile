d67b21acd256dc0a47ccde4a6781bae57fde2497
kokoaj <kokiebisu@icloud.com>
2022-10-23 16:30:58 -0700

    fix

--- Makefile
+++ Makefile
@@ -7,3 +7,11 @@ build:
 	cd packages/service-payment && make build && cd ../.. &&
 	cd packages/service-authentication && make build && cd ../.. &&
 	cd packages/service-authorizer && make build && cd ../..
+
+.PHONY: deploy-local
+deploy-local:
+	./scripts/deploy.sh local
+
+.PHONY: deploy-dev
+deploy-dev:
+	./scripts/deploy.sh dev
---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+ENVIRONMENT=$1
+echo "ENVIRONMENT: $ENVIRONMENT"
+
+if test -f "serverless.yml"; then
+    echo "Removing serverless.yml"
+    rm serverless.yml 
+fi
+
+if $ENVIRONMENT == 'dev'; then
+    echo "Copying serverless.dev.yml"
+    cp config/serverless-compose.dev.yml ./serverless-compose.yml;
+else
+    echo "Copying serverless.yml"
+    cp config/serverless-compose.yml ./serverless-compose.yml;
+fi
+npx sls deploy --stage local;
