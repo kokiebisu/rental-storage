0159622c385ae34577a81887ad173c83d0e6c755
kokoaj <kokiebisu@icloud.com>
2022-10-25 09:12:44 -0700

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,31 +4,31 @@ npm install
 
 # typescript
 echo "Installing Booking Service dependencies..."
-cd packages/service-booking && npm install && cd ../..
+cd packages/service-booking && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 echo "Installing Listing Service dependencies..."
-cd packages/service-listing && npm install && cd ../..
+cd packages/service-listing && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 echo "Installing User Service dependencies..."
-cd packages/service-user && npm install && cd ../..
+cd packages/service-user && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 
 # python
 echo "Installing Image Service dependencies..."
-cd packages/service-image && npm install && cd ../..
+cd packages/service-image && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 echo "Installing Slack Service dependencies..."
-cd packages/service-slack && npm install && cd ../..
+cd packages/service-slack && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 
 # golang
 echo "Installing Authentication Service dependencies..."
-cd packages/service-authentication && npm install && cd ../..
+cd packages/service-authentication && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 echo "Installing Authorizer Service dependencies..."
-cd packages/service-authorizer && npm install && cd ../..
+cd packages/service-authorizer && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 echo "Installing Payment Service dependencies..."
-cd packages/service-payment && npm install && cd ../..
+cd packages/service-payment && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 
 echo "Installing Infrastructure dependencies..."
-cd infrastructure && npm install && npx serverless plugin install -n serverless-deployment-bucket && cd ..
+cd infrastructure && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ..
 
 echo "Installing Appsync dependencies..."
-cd api/appsync && npm install && npx serverless plugin install -n serverless-deployment-bucket && cd ../..
+cd api/appsync && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 echo "Installing Api Gateway dependencies..."
-cd api/apigateway && npm install && npx serverless plugin install -n serverless-deployment-bucket && cd ../..
+cd api/apigateway && npm install && npx serverless plugin install -n serverless-deployment-bucket serverless-export-env && cd ../..
 
