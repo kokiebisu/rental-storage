1d53233dc9e777268d97a6484062008195f95f68
kokoaj <kokiebisu@icloud.com>
2022-10-24 22:11:47 -0700

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,34 +1,34 @@
 #! /bin/sh
 echo "Installing package root dependencies..."
-npm ci
+npm install
 
 # typescript
 echo "Installing Booking Service dependencies..."
-cd packages/service-booking && npm ci && cd ../..
+cd packages/service-booking && npm install && cd ../..
 echo "Installing Listing Service dependencies..."
-cd packages/service-listing && npm ci && cd ../..
+cd packages/service-listing && npm install && cd ../..
 echo "Installing User Service dependencies..."
-cd packages/service-user && npm ci && cd ../..
+cd packages/service-user && npm install && cd ../..
 
 # python
 echo "Installing Image Service dependencies..."
-cd packages/service-image && npm ci && cd ../..
+cd packages/service-image && npm install && cd ../..
 echo "Installing Slack Service dependencies..."
-cd packages/service-slack && npm ci && cd ../..
+cd packages/service-slack && npm install && cd ../..
 
 # golang
 echo "Installing Authentication Service dependencies..."
-cd packages/service-authentication && npm ci && cd ../..
+cd packages/service-authentication && npm install && cd ../..
 echo "Installing Authorizer Service dependencies..."
-cd packages/service-authorizer && npm ci && cd ../..
+cd packages/service-authorizer && npm install && cd ../..
 echo "Installing Payment Service dependencies..."
-cd packages/service-payment && npm ci && cd ../..
+cd packages/service-payment && npm install && cd ../..
 
 echo "Installing Infrastructure dependencies..."
-cd infrastructure && npm ci && cd ..
+cd infrastructure && npm install && cd ..
 
 echo "Installing Appsync dependencies..."
-cd api/appsync && npm ci && cd ../..
+cd api/appsync && npm install && cd ../..
 echo "Installing Api Gateway dependencies..."
-cd api/apigateway && npm ci && cd ../..
+cd api/apigateway && npm install && cd ../..
 
