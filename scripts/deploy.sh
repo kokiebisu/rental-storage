cb2c37efb728dcc2ab2c4b8792ced55d3d3a0171
kokoaj <kokiebisu@icloud.com>
2022-10-26 08:39:37 -0700

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,31 @@ ENVIRONMENT=$1
 echo "ENVIRONMENT: $ENVIRONMENT"
 
 if [ -e serverless-compose.yml ]; then
-    echo "Removing serverless-compose.yml"
-    rm serverless-compose.yml 
+    echo "Removing serverless-compose.yml";
+    rm serverless-compose.yml ;
 fi
 
 if [ "$ENVIRONMENT" = "dev" ]; then
-    echo "Copying serverless.dev.yml"
+    echo "Copying serverless.dev.yml";
     cp config/serverless-compose.dev.yml ./serverless-compose.yml;
 else
-    echo "Copying serverless.yml"
+    echo "Copying serverless.yml";
     cp config/serverless-compose.yml ./serverless-compose.yml;
 fi
 
-cd api/appsync && npm install && cd ../..;
+# Install dev dependencies
+cd api/appsync && npm ci --only=dev && cd ../.. &&
+cd api/apigateway && npm ci --only=dev && cd ../.. &&
+
+cd infrastructure && npm ci --only=dev && cd ../.. &&
+
+cd packages/service-authentication && npm ci --only=dev && cd ../.. &&
+cd packages/service-authorizer && npm ci --only=dev && cd ../.. &&
+cd packages/service-booking && npm ci --only=dev && cd ../.. &&
+cd packages/service-image && npm ci --only=dev && cd ../.. &&
+cd packages/service-listing && npm ci --only=dev && cd ../.. &&
+cd packages/service-payment && npm ci --only=dev && cd ../.. &&
+cd packages/service-slack && npm ci --only=dev && cd ../.. &&
+cd packages/service-user && npm ci --only=dev && cd ../.. &&
 
 npx sls deploy --stage $ENVIRONMENT;
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,35 +3,35 @@ echo "Installing package root dependencies..."
 npm install
 
 echo "Installing Infrastructure dependencies..."
-cd infrastructure && npm install && cd ..
+cd infrastructure && npm ci --only=prod && cd ..
 
 # api
 echo "Installing Appsync dependencies..."
-cd api/appsync && npm install && cd ../..
+cd api/appsync && npm ci --only=prod && cd ../..
 echo "Installing Api Gateway dependencies..."
-cd api/apigateway && npm install && cd ../..
+cd api/apigateway && npm ci --only=prod && cd ../..
 
 
 # typescript
 echo "Installing Booking Service dependencies..."
-cd packages/service-booking && npm install && cd ../..
+cd packages/service-booking && npm ci --only=prod && cd ../..
 echo "Installing Listing Service dependencies..."
-cd packages/service-listing && npm install && cd ../..
+cd packages/service-listing && npm ci --only=prod && cd ../..
 echo "Installing User Service dependencies..."
-cd packages/service-user && npm install && cd ../..
+cd packages/service-user && npm ci --only=prod && cd ../..
 
 # python
 echo "Installing Image Service dependencies..."
-cd packages/service-image && npm install && cd ../..
+cd packages/service-image && npm ci --only=prod && cd ../..
 echo "Installing Slack Service dependencies..."
-cd packages/service-slack && npm install && cd ../..
+cd packages/service-slack && npm ci --only=prod && cd ../..
 
 # golang
 echo "Installing Authentication Service dependencies..."
-cd packages/service-authentication && npm install && cd ../..
+cd packages/service-authentication && npm ci --only=prod && cd ../..
 echo "Installing Authorizer Service dependencies..."
-cd packages/service-authorizer && npm install && cd ../..
+cd packages/service-authorizer && npm ci --only=prod && cd ../..
 echo "Installing Payment Service dependencies..."
-cd packages/service-payment && npm install && cd ../..
+cd packages/service-payment && npm ci --only=prod && cd ../..
 
 
