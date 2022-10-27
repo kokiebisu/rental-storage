e8ccc11e083ba9105afbd3a3d7998ff846f23b7f
kokoaj <kokiebisu@icloud.com>
2022-10-27 08:39:30 -0700

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,30 +17,30 @@ else
 fi
 
 # Install dev dependencies
-echo "Install dev dependencies for appsync" && 
-cd api/appsync && npm install -D && cd ../.. &&
-echo "Install dev dependencies for apigateway" && 
-cd api/apigateway && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for appsync" && 
+# cd api/appsync && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for apigateway" && 
+# cd api/apigateway && npm install -D && cd ../.. &&
 
-echo "Install dev dependencies for infrastructure" && 
-cd infrastructure && npm install -D && cd .. &&
+# echo "Install dev dependencies for infrastructure" && 
+# cd infrastructure && npm install -D && cd .. &&
 
-echo "Install dev depdendencies for authentication service" &&
-cd packages/service-authentication && npm install -D && cd ../.. &&
-echo "Install dev depdendencies for authorizer service" &&
-cd packages/service-authorizer && npm install -D && cd ../.. &&
-echo "Install dev depdendencies for booking service" &&
-cd packages/service-booking && npm install -D && cd ../.. &&
-echo "Install dev dependencies for image service" &&
-cd packages/service-image && npm install -D && cd ../.. &&
-echo "Install dev dependencies for listing service" &&
-cd packages/service-listing && npm install -D && cd ../.. &&
-echo "Install dev dependencies for payment service" &&
-cd packages/service-payment && npm install -D && cd ../.. &&
-echo "Install dev dependencies for slack service" &&
-cd packages/service-slack && npm install -D && cd ../.. &&
-echo "Install dev dependencies for user service" &&
-cd packages/service-user && npm install -D && cd ../.. &&
+# echo "Install dev depdendencies for authentication service" &&
+# cd packages/service-authentication && npm install -D && cd ../.. &&
+# echo "Install dev depdendencies for authorizer service" &&
+# cd packages/service-authorizer && npm install -D && cd ../.. &&
+# echo "Install dev depdendencies for booking service" &&
+# cd packages/service-booking && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for image service" &&
+# cd packages/service-image && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for listing service" &&
+# cd packages/service-listing && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for payment service" &&
+# cd packages/service-payment && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for slack service" &&
+# cd packages/service-slack && npm install -D && cd ../.. &&
+# echo "Install dev dependencies for user service" &&
+# cd packages/service-user && npm install -D && cd ../.. &&
 
 echo "Listing the files..."
 ls
@@ -48,4 +48,4 @@ ls
 TEST=$(pwd)
 echo $TEST
 echo "Deploy $ENVIRONMENT" && 
-npx sls deploy --stage $ENVIRONMENT;
+serverless deploy --stage $ENVIRONMENT;
