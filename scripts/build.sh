3725f709110dbb8a06a06680d46ce977bad5b864
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-10-29 13:47:16 -0700

    146 Refactor: Migrate to PNPM (#147)

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Go
-(cd packages/service-authorizer && make prepare || { echo 'failed' ; exit 1; });
-(cd packages/service-authentication && make prepare || { echo 'failed' ; exit 1; });
-(cd packages/service-payment && make prepare || { echo 'failed' ; exit 1; });
-
-# Typescript
-(turbo run build || { echo 'failed' ; exit 1; });
