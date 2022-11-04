dada1763891bb14528ce02e8093d741ae7a259fd
kokoaj <kokiebisu@icloud.com>
2022-11-03 18:59:19 -0700

    fix merge conflicts

--- packages/service-user/src/adapter/api/controller/FindUserByEmail.ts
+++ packages/service-user/src/adapter/api/controller/FindUserByEmail.ts
@@ -4,13 +4,9 @@ export const handler = async (event: any) => {
   console.log("FIND USER BY EMAIL EVENT: ", event);
   const { email } = event.queryStringParameters;
   const service = await UserServiceImpl.create();
-<<<<<<< HEAD
-  return service.findByEmail(email);
-=======
   const userDTO = service.findByEmail(email);
   return {
     statusCode: 200,
     body: JSON.stringify(userDTO),
   };
->>>>>>> 313874a240b3c3f9b8e03735c93949ca1277492f
 };
