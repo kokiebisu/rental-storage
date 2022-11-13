ff76d365c9be46c7c2b4240a9e80f5aecd36f0bd
kokoaj <kokiebisu@icloud.com>
2022-11-13 00:48:55 -0800

    refactor user service

--- packages/service-user/src/adapter/api/graphql/FindUserByEmail.ts
+++
@@ -1,7 +0,0 @@
-import { UserServiceImpl } from "../../service";
-
-export const handler = async (event: any) => {
-  const { email } = event.arguments;
-  const service = await UserServiceImpl.create();
-  return service.findByEmail(email);
-};
