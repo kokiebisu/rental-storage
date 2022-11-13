2d160eb396aa94e07cfc40b40183e16f614e5bbc
kokoaj <kokiebisu@icloud.com>
2022-11-13 00:46:22 -0800

    refactor listing service

--- packages/service-listing/src/adapter/api/graphql/FindMyListings.ts
+++
@@ -1,7 +0,0 @@
-import { ListingServiceImpl } from "../../service";
-
-export const handler = async (event: any) => {
-  const { uid } = event.identity.resolverContext;
-  const service = await ListingServiceImpl.create();
-  return service.findListingsByUserId(uid);
-};
