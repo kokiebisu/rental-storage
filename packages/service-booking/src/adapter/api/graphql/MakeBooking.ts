46bfa5d5b6d7118dae28b4c31f5126353ebc30c9
kokoaj <kokiebisu@icloud.com>
2022-11-13 00:47:08 -0800

    refactor booking service

--- packages/service-booking/src/adapter/api/graphql/MakeBooking.ts
+++
@@ -1,7 +0,0 @@
-import { BookingServiceImpl } from "../../service";
-
-export const handler = async (event: any) => {
-  const { amount, currency, userId, listingId, items } = event.arguments;
-  const service = await BookingServiceImpl.create();
-  return await service.makeBooking(amount, currency, userId, listingId, items);
-};
