b0cabcc9f9e0f837308f04a2c294bd4fa9f93c38
kokoaj <kokiebisu@icloud.com>
2022-11-15 08:57:47 -0800

    remove typescript

--- packages/service-user/src/adapter/api/controller/FindUserByUserId.ts
+++
@@ -1,8 +0,0 @@
-import { UserServiceImpl } from "../../service";
-
-export const handler = async (event: any) => {
-  const { userId } = event.pathParameters;
-  const service = await UserServiceImpl.create();
-  const userDTO = await service.findById(userId);
-  return { statusCode: 200, body: JSON.stringify(userDTO) };
-};
--- packages/service-user/src/port/sender-message/UserMessageSender.ts
+++
@@ -1,5 +0,0 @@
-import { UserInterface } from "../../domain/types";
-
-export interface UserMessageSender {
-  userCreated(data: UserInterface): Promise<void>;
-}
