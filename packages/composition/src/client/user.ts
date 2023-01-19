8efbd945e498e1bac8ae18b6c11ce6189e281b20
kokoaj <kokiebisu@icloud.com>
2023-01-18 22:12:23 -0800

    fix: authorizer always returning 401 (#184)

--- packages/composition/src/client/user.ts
+++ packages/composition/src/client/user.ts
@@ -2,15 +2,13 @@ import { BaseRestClient } from "./base";
 
 export class UserRestClient extends BaseRestClient {
   public async findUserById(userId: string) {
-    console.log("FIND USER BY ID: ", userId);
     const response = await this.client.get(`/users/${userId}`);
-    console.log("FINDUSERBYID RESPONSE: ", response);
     return response.data;
   }
 
   public async findByEmail(email: string) {
     const response = await this.client.get(
-      `/users/find-by-email/emailAddress=${email}`
+      `/users/find-by-email?emailAddress=${email}`
     );
     return response.data;
   }
