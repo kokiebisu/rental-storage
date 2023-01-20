e68fd30720665895080b00c419ff2e329cd8c3f6
kokoaj <kokiebisu@icloud.com>
2023-01-19 23:50:25 -0800

    add type to user, listing model

--- packages/service-slack/domain/user.py
+++ packages/service-slack/domain/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, first_name, last_name, email_address) -> None:
+    def __init__(self, first_name: str, last_name: str, email_address: str) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.email_address = email_address
