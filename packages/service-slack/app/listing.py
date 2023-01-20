93a213e547803726396cb1f6e7dc1cbbc222b01d
kokoaj <kokiebisu@icloud.com>
2023-01-19 23:51:09 -0800

    set up base test cases

--- packages/service-slack/app/listing.py
+++ packages/service-slack/app/listing.py
@@ -17,11 +17,18 @@ class SlackListingMessageSenderService:
                     street_address=message['streetAddress']
                 )
         if event_name == "created":
-            self._alert_listing_created(listing)
+            self._alert(entity=listing)
         else:
             raise EventNameNotFoundException
 
-    def _alert_listing_created(self, entity: Listing):
+    def _alert(self, entity: Listing):
         channel_name = 'C0464PCNZH8'
-        message = f'Listing was posted! :tada:\n>Lender Id: {entity.lender_id}\n>Street Address: {entity.street_address}'
+        message = SlackListingMessageSenderService._generate_listing_created_message(lender_id=entity["lender_id"], street_address=entity["street_address"])
         self.bot.send_chat_message(channel_name=channel_name, message=message)
+    
+    def _generate_listing_created_message(self, lender_id: str, street_address: str):
+        return (
+            f'Listing was posted! :tada:\n'
+            f'Lender Id: {lender_id}\n'
+            f'Street Address: {street_address}\n'
+        )
