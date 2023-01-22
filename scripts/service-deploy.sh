797a5d51c5bc0ec7366f5ecb08c95de03bc4cc3a
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-01-22 12:10:23 -0800

    added bash command in npm scripts

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -4,21 +4,21 @@ function deploy_services () {
     local packages=("image" "listing" "booking" "slack" "user" "authentication" "authorizer")
     for package in "${packages[@]}"
     do
-        echo "Deploying ${(C)package} service...";
-        (cd "packages/service-$package" && pnpm run deploy);
+        echo "Deploying ${package} service...";
+        (cd "packages/service-${package}" && pnpm run deploy);
     done
 }
 
 function deploy_composition () {
     local package="composition"
-    echo "Deploying ${(C)package}...";
-    (cd "packages/$package" && pnpm run deploy);
+    echo "Deploying ${package}...";
+    (cd "packages/${package}" && pnpm run deploy);
 }
 
 function deploy_appsync() {
     local package="appsync"
-    echo "Deploying ${(C)package}...";
-    (cd "$package" && pnpm run deploy);
+    echo "Deploying ${package}...";
+    (cd "${package}" && pnpm run deploy);
 }
 
 deploy_services
