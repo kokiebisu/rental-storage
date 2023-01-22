d6282a30009bf5b7768c03534407e02170694c2a
kokoaj <kokiebisu@icloud.com>
2023-01-22 12:56:53 -0800

    remove composition fragment

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -21,14 +21,6 @@ function install_dependencies_appsync() {
     (cd appsync && pnpm install);
 }
 
-function install_dependencies_composition() {
-    local package="composition"
-    name=`echo ${package:0:1} | tr  '[a-z]' '[A-Z]'`${package:1}
-    echo "Installing ${name} dependencies...";
-    (cd packages/composition && pnpm install);
-}
-
 install_dependencies_root
 install_dependencies_services
 install_dependencies_appsync
-install_dependencies_composition
