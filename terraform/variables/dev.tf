9504fc00726dd294fbf1c406727961a8a94ed791
kokoaj <kokiebisu@icloud.com>
2023-01-22 17:26:57 -0800

    fix

--- terraform/variables/dev.tf
+++ terraform/variables/dev.tf
@@ -8,6 +8,11 @@ variable "booking_table_name" {
   type        = string
 }
 
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
 variable "environment" {
   description = "Either dev/local/production"
   type        = string
