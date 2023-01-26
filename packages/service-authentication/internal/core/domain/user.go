1f58ed37f2c7ae26f9a85f96cbdabb473083b78a
kokoaj <kokiebisu@icloud.com>
2023-01-25 20:14:50 -0800

    initial

--- packages/service-authentication/internal/core/domain/user.go
+++ packages/service-authentication/internal/core/domain/user.go
@@ -1,11 +1,11 @@
 package domain
 
 type User struct {
-    Id int `json:"id"`
-    Uid string `json:"uid"`
-    EmailAddress string `json:"emailAddress"`
-    Password string `json:"password"`
-    FirstName string `json:"firstName"`
-    LastName string `json:"lastName"`
-    CreatedAt string `json:"createdAt"`
+	Id           int8   `json:"id"`
+	Uid          string `json:"uid"`
+	EmailAddress string `json:"emailAddress"`
+	Password     string `json:"password"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	CreatedAt    string `json:"createdAt"`
 }
