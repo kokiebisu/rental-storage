aa4eed148d33230c80e50e45648c196ea04e44eb
kokoaj <kokiebisu@icloud.com>
2023-01-27 20:18:50 -0800

    initial

--- packages/service-authorizer/cmd/authorize/handler.go
+++ packages/service-authorizer/cmd/authorize/handler.go
@@ -11,7 +11,8 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	"service-authorizer/pkg/port"
+	responses "github.com/kokiebisu/rental-storage/service-authorizer/internal"
+	"github.com/kokiebisu/rental-storage/service-authorizer/internal/core/port"
 )
 
 func HandleRequest(ctx context.Context, event port.Event) (*events.AppSyncLambdaAuthorizerResponse, error) {
@@ -23,37 +24,22 @@ func HandleRequest(ctx context.Context, event port.Event) (*events.AppSyncLambda
 	}
 	encodedPayload, err := json.Marshal(&body)
 	if err != nil {
-		return generateUnauthorizedResponse(), nil
+		return responses.SendUnAuthorizedResponse(), nil
 	}
 	authenticationEndpoint := fmt.Sprintf("%s/auth/verify", os.Getenv("SERVICE_API_ENDPOINT"))
 	// send REST API to verify jwt
 	resp, err := http.Post(authenticationEndpoint, "application/json", bytes.NewBuffer(encodedPayload))
 	if err != nil {
-		return generateUnauthorizedResponse(), nil
+		return responses.SendUnAuthorizedResponse(), nil
 	}
 	payload := struct {
 		UId string `json:"uid"`
 		Exp int    `json:"exp"`
 	}{}
 	if err = json.NewDecoder(resp.Body).Decode(&payload); err != nil {
-		return generateUnauthorizedResponse(), nil
+		return responses.SendUnAuthorizedResponse(), nil
 	}
-	return generateAuthorizedResponse(payload.UId), nil
-}
-
-func generateUnauthorizedResponse() *events.AppSyncLambdaAuthorizerResponse {
-	return &events.AppSyncLambdaAuthorizerResponse{
-		IsAuthorized: false,
-	}
-}
-
-func generateAuthorizedResponse(uid string) *events.AppSyncLambdaAuthorizerResponse {
-	return &events.AppSyncLambdaAuthorizerResponse{
-		IsAuthorized: true,
-		ResolverContext: map[string]interface{}{
-			"uid": uid,
-		},
-	}
+	return responses.SendAuthorizedResponse(payload.UId), nil
 }
 
 func main() {
---
+++ packages/service-authorizer/internal/response.go
@@ -0,0 +1,20 @@
+package responses
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func SendUnAuthorizedResponse() *events.AppSyncLambdaAuthorizerResponse {
+	return &events.AppSyncLambdaAuthorizerResponse{
+		IsAuthorized: false,
+	}
+}
+
+func SendAuthorizedResponse(uid string) *events.AppSyncLambdaAuthorizerResponse {
+	return &events.AppSyncLambdaAuthorizerResponse{
+		IsAuthorized: true,
+		ResolverContext: map[string]interface{}{
+			"uid": uid,
+		},
+	}
+}
