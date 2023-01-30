dc6559e6ef1b1f5df97f5baa08d07242a691345c
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2023-01-30 09:37:11 -0800

    feat(authentication): add reason field to customerror struct literal for more details of the error (#274)

--- packages/service-authentication/internal/error/custom.go
+++ packages/service-authentication/internal/error/custom.go
@@ -2,9 +2,22 @@ package errors
 
 type CustomError struct {
 	StatusCode uint16
-	Err        error
+	Message    string
+	Reason     error
+}
+
+type ErrorResponsePayload struct {
+	Message string `json:"message"`
+	Reason  error  `json:"reason"`
 }
 
 func (e *CustomError) Error() string {
-	return e.Err.Error()
+	return e.Reason.Error()
+}
+
+func (e *CustomError) GetPayload() ErrorResponsePayload {
+	return ErrorResponsePayload{
+		Message: e.Message,
+		Reason: e.Reason,
+	}
 }
