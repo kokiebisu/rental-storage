2ce6896504f8754521b11e4d399dcc4d6a00c2d8
kokoaj <kokiebisu@icloud.com>
2023-01-29 21:48:14 -0800

    fix lint

--- packages/service-slack/app/listing.py
+++ packages/service-slack/app/listing.py
@@ -20,9 +20,9 @@ class SlackListingMessageSenderService:
                 )
         if event_name in ListingEvents:
             message = SlackListingMessageSenderService\
-                 .generate_listing_created_message(
-                     lender_id=listing.lender_id,
-                     street_address=listing.street_address)
+                .generate_listing_created_message(
+                    lender_id=listing.lender_id,
+                    street_address=listing.street_address)
             self.bot.send_chat_message(channel_name=channel_name,
                                        message=message)
         else:
