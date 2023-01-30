51d558e5c3cafbb29127d18492532acfe85f0fdd
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-01-29 21:40:11 -0800

    add Error to Custom Error

--- packages/service-authentication/test/error/handler_test.go
+++ packages/service-authentication/test/error/handler_test.go
@@ -1,16 +1,18 @@
 package test
 
 import (
+	"errors"
 	"testing"
 
-	errors "github.com/kokiebisu/rental-storage/service-authentication/internal/error"
+	errs "github.com/kokiebisu/rental-storage/service-authentication/internal/error"
 	"github.com/stretchr/testify/assert"
 )
 
 // for a valid return value.
 func TestCustomError(t *testing.T) {
 	msg := "something went wrong"
-	err := errors.ErrorHandler.CustomError(msg)
-	assert.Equal(t, uint16(500), err.StatusCode)
-	assert.Equal(t, msg, err.Error())
+	err := errors.New(msg)
+	customError := errs.ErrorHandler.CustomError(msg, err)
+	assert.Equal(t, uint16(500), customError.StatusCode)
+	assert.Equal(t, msg, customError.Error())
 }
