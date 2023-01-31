94222b2a466a7879e6a888c503b2c834fab48071
kokoaj <kokiebisu@icloud.com>
2023-01-31 14:48:52 -0800

    initial

---
+++ web/src/components/pages/home/hook.ts
@@ -0,0 +1,23 @@
+import { Space } from "@/types/interface";
+
+export default () => {
+  const spaces: Space[] = [];
+  const space: Space = {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  };
+  // temporary data
+  const generateMockSpaces = () => {
+    for (let i = 0; i < 20; i++) {
+      spaces.push(space);
+    }
+  };
+  generateMockSpaces();
+  return { spaces };
+};
