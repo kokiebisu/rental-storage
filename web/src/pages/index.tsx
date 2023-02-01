af593baac9151390e799306f8e146f18adfccd8e
kokoaj <kokiebisu@icloud.com>
2023-01-31 16:18:21 -0800

    fix build

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
