0b5016b48e4fc6395278e2f92960db49ea713fcd
kokoaj <kokiebisu@icloud.com>
2023-02-01 22:49:24 -0800

    fix Hydration error

--- web/src/components/card/index.tsx
+++ web/src/components/card/index.tsx
@@ -5,7 +5,7 @@ export interface SpaceCardProps {
 }
 
 export const SpaceCard = ({ space }: SpaceCardProps) => (
-  <div key={space.id} className="group relative">
+  <div className="group relative">
     <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
       <img
         src={space.imageSrc}
@@ -16,10 +16,8 @@ export const SpaceCard = ({ space }: SpaceCardProps) => (
     <div className="mt-4 flex justify-between">
       <div>
         <h3 className="text-sm text-gray-700">
-          <a href={space.href}>
-            <span aria-hidden="true" className="absolute inset-0" />
-            {space.name}
-          </a>
+          <span aria-hidden="true" className="absolute inset-0" />
+          {space.name}
         </h3>
         <p className="mt-1 text-sm text-gray-500">{space.color}</p>
       </div>
