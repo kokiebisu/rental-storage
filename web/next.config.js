8cb814b4dbefeb6671b7653cd0be71db6913b8bb
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-02-02 21:15:10 -0800

    display google map using api on a landing page

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   eslint: {
     dirs: ["pages", "types", "components"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+  env: {
+    GOOGLE_MAP_API_KEY: process.env.GOOGLE_MAP_API_KEY,
   },
 };
 
---
+++ web/src/components/map/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const googleMapAPIKey = process.env.GOOGLE_MAP_API_KEY as string;
+
+const AnyReactComponent = ({ text }: { text: string }) => <div>{text}</div>;
+
+export default function SimpleMap() {
+  // user's lat and lng
+  const defaultProps = {
+    center: {
+      lat: 49.2827,
+      lng: -123.1207,
+    },
+    zoom: 11,
+  };
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: googleMapAPIKey }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
+}
