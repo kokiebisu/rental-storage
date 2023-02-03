5f4bea2f4300793960cd86206aabfa51ad7373f8
kokoaj <kokiebisu@icloud.com>
2023-02-02 21:57:16 -0800

    fix

--- web/src/context/auth.tsx
+++
@@ -1,8 +0,0 @@
-export const AuthContext = createContext({
-  authState: {
-    isLoading: false,
-  },
-  signOut: () => Promise<void>,
-  signIn: (data: any) => Promise<void>,
-  signUp: (data: any) => Promise<void>,
-});
