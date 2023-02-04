a4ef92fac1373592d016a818a0be916a6057aa36
kokoaj <kokiebisu@icloud.com>
2023-02-04 11:48:28 -0800

    set up jest global setup script

---
+++ packages/composition/test/setup.ts
@@ -0,0 +1,35 @@
+require("dotenv/config");
+require("ts-node").register({
+  transpileOnly: true,
+});
+
+import { faker } from "@faker-js/faker";
+import { UserRestClient } from "../src/client";
+
+let mockEmailAddress = faker.internet.email();
+let mockFirstName = faker.name.firstName();
+let mockLastName = faker.name.lastName();
+let mockPassword = faker.internet.password();
+
+module.exports = async function (globalConfig: any) {
+  const uid = await registerUser();
+
+  (global as any).data = {
+    uid,
+    mockEmailAddress,
+    mockFirstName,
+    mockLastName,
+    mockPassword,
+  };
+};
+
+const registerUser = async function () {
+  const userClient = new UserRestClient();
+  const responseData = await userClient.createUser(
+    mockEmailAddress,
+    mockFirstName,
+    mockLastName,
+    mockPassword
+  );
+  return responseData.uid;
+};
