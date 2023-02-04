0ae6cbcf64053df7a0e7c1cdf6502eeba2658529
kokoaj <kokiebisu@icloud.com>
2023-02-03 19:26:21 -0800

    initial

---
+++ packages/service-authentication/test/service/token_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/kokiebisu/rental-storage/service-authentication/internal/core/service"
+	"github.com/kokiebisu/rental-storage/service-authentication/test/data"
+)
+
+func TestGenerateTokenSuccess(t *testing.T) {
+	tokenService := service.NewTokenService()
+	token, err := tokenService.GenerateToken(data.MockUId)
+	if err != nil {
+		panic("something went wrong")
+	}
+	assert.Greater(t, len(token), 0, "token should have a length greater than 0")
+	assert.Nil(t, err, "should be no errors")
+}
+
+func TestVerifyTokenSuccess(t *testing.T) {
+	tokenService := service.NewTokenService()
+	token, err := tokenService.GenerateToken(data.MockUId)
+	assert.Nil(t, err, "should be no errors")
+	claims, err := tokenService.VerifyToken(token)
+	assert.Greater(t, len(claims.UId), 0, "should be valid uid")
+	assert.Nil(t, err, "should be no errors")
+}
