d273b47c207bb2f9890fc9e0cf285f78ecb7f452
kokoaj <kokiebisu@icloud.com>
2023-02-08 20:52:11 -0800

    fix addListing mutation

--- packages/composition/test/mutation/addListing.test.ts
+++ packages/composition/test/mutation/addListing.test.ts
@@ -1,5 +1,20 @@
+import { ListingRestClient } from "../../src/client";
+
 describe("addListing()", () => {
   it("should work with valid input", async () => {
     expect(global.data.listingId).not.toBeUndefined();
+  });
+
+  it.todo("wrong input", () => {
+    const listingClient = new ListingRestClient();
+    //   title: 'HUIHIU',
+    // streetAddress: 'IHI',
+    // latitude: 1.5,
+    // longitude: 1.5,
+    // imageUrls: [ 'UIGIU' ],
+    // feeType: 'MONTHLY',
+    // feeAmount: 10,
+    // feeCurrency: 'CAD'
+    // await listingClient.addListing()
   });
 });
