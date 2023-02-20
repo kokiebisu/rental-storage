4fc6bca2bb786727d2402c9b99fcf3f8a4f6a6a4
kokoaj <kokiebisu@icloud.com>
2023-02-20 14:25:47 -0800

    feat(ci): set up github actions to destroy dev infrastructure (#424)

--- scripts/terraform-setup.sh
+++ scripts/terraform-setup.sh
@@ -35,26 +35,6 @@ function setup_terraform_variables() {
     (cp terraform/config/${ENVIRONMENT}/variables.tf terraform/variables.tf);
 }
 
-function setup_terraform_backend() {
-    local ENVIRONMENT=$1
-    local FILE="terraform/backend.tf"
-    if [ -f "$FILE" ]; then
-        echo "backend.tf does exist"
-        (rm terraform/backend.tf)
-    fi
-    echo "Setting up backend.tf for ${ENVIRONMENT} environment..."
-    (cp terraform/config/${ENVIRONMENT}/backend.tf terraform/backend.tf)
-}
-
-function remove_terraform_backend() {
-    local FILE="terraform/backend.tf"
-    if [ -f "$FILE" ]; then
-        echo "backend.tf does exist"
-        echo "Removing backend.tf..."
-        (rm terraform/backend.tf)
-    fi
-}
-
 setup_terraform_versions $1
 setup_terraform_providers $1
 setup_terraform_variables $1
@@ -62,11 +42,9 @@ setup_terraform_variables $1
 if [ "$1" = "local" ]
 then
     echo "Setting up for local deployment"
-    remove_terraform_backend $1
 fi
 
 if [ "$1" = "dev" ] || [ "$1" = "production" ]
 then
     echo "Setting up for dev/production deployment..."
-    setup_terraform_backend $1
 fi
