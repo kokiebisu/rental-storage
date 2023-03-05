fcb7f7356e239e425961246c583266201d474636
kokiebisu <kokiebisu@icloud.com>
2023-03-04 19:59:11 -0700

    test

--- packages/service-user/test/integration/main_test.go
+++ packages/service-user/test/integration/main_test.go
@@ -1,10 +1,17 @@
 package integration
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
 	"github.com/kokiebisu/rental-storage/service-user/internal/adapter/publisher"
 	"github.com/kokiebisu/rental-storage/service-user/internal/adapter/repository"
 	"github.com/kokiebisu/rental-storage/service-user/internal/client"
@@ -16,6 +23,7 @@ import (
 
 func TestMain(m *testing.M) {
 	setup()
+	streamSetup()
 	code := m.Run()
 	teardown()
 	os.Exit(code)
@@ -42,6 +50,51 @@ func setup() {
 		os.Exit(1)
 	}
 	data.UserPublisher = publisher.NewUserPublisher(data.KinesisClient)
+}
+
+func streamSetup() {
+	ctx := context.Background()
+	var err error
+	// Create a new Kinesis stream
+	streamName := "local-EventStream"
+	shardCount := int32(1)
+	_, err = data.KinesisClient.CreateStream(ctx, &kinesis.CreateStreamInput{
+		StreamName: aws.String(streamName),
+		ShardCount: aws.Int32(shardCount),
+	})
+	if err != nil {
+		var aerr *types.ResourceInUseException
+		if errors.As(err, &aerr) {
+			log.Println("Stream already exists:", aerr)
+		} else {
+			log.Fatalf(err.Error())
+		}
+	}
+
+	// Wait for the stream to become active
+	streamStatus := ""
+	for streamStatus != "ACTIVE" {
+		stream, err := data.KinesisClient.DescribeStream(ctx, &kinesis.DescribeStreamInput{
+			StreamName: aws.String(streamName),
+		})
+		if err != nil {
+			fmt.Println("Failed to describe Kinesis stream:", err)
+			os.Exit(1)
+		}
+		streamStatus = string(stream.StreamDescription.StreamStatus)
+		time.Sleep(1 * time.Second)
+	}
+
+	// Put a record into the stream
+	partitionKey := "123"
+	_, err = data.KinesisClient.PutRecord(ctx, &kinesis.PutRecordInput{
+		StreamName:   aws.String(streamName),
+		PartitionKey: aws.String(partitionKey),
+		Data:         []byte("Hello, world!"),
+	})
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 }
 
 // Close the database connection
