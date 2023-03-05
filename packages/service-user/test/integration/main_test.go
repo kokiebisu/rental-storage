1912c51ba3df5e7f0d2b5e4d1734faacca5e6a0c
kokiebisu <kokiebisu@icloud.com>
2023-03-04 18:22:16 -0700

    minor fix

--- packages/service-user/test/integration/main_test.go
+++ packages/service-user/test/integration/main_test.go
@@ -1,26 +1,17 @@
 package integration
 
 import (
-	"database/sql"
 	"log"
 	"os"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/service/kinesis"
 	"github.com/kokiebisu/rental-storage/service-user/internal/adapter/publisher"
 	"github.com/kokiebisu/rental-storage/service-user/internal/adapter/repository"
 	"github.com/kokiebisu/rental-storage/service-user/internal/client"
-	"github.com/kokiebisu/rental-storage/service-user/internal/core/port"
 	customerror "github.com/kokiebisu/rental-storage/service-user/internal/error"
+	"github.com/kokiebisu/rental-storage/service-user/test/data"
 
 	_ "github.com/lib/pq"
-)
-
-var (
-	pc            *sql.DB
-	UserRepo      *repository.UserRepository
-	kinesisClient *kinesis.Client
-	UserPublisher port.UserPublisher
 )
 
 func TestMain(m *testing.M) {
@@ -33,28 +24,28 @@ func TestMain(m *testing.M) {
 func setup() {
 	var err *customerror.CustomError
 	// Start a PostgreSQL container
-	pc, err = client.GetPostgresClient()
+	data.PostgresClient, err = client.GetPostgresClient()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	UserRepo = repository.NewUserRepository(pc)
+	data.UserRepository = repository.NewUserRepository(data.PostgresClient)
 	// Set up tables
-	err = UserRepo.Setup()
+	err = data.UserRepository.Setup()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	kinesisClient, err = client.GetKinesisClient()
+	data.KinesisClient, err = client.GetKinesisClient()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	UserPublisher = publisher.NewUserPublisher(kinesisClient)
+	data.UserPublisher = publisher.NewUserPublisher(data.KinesisClient)
 }
 
 // Close the database connection
 func teardown() {
 	// dropTables()
-	pc.Close()
+	data.PostgresClient.Close()
 }
