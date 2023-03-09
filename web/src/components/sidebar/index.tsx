657d925650965708ea695a3289ebf50f1f03630c
kokiebisu <kokiebisu@icloud.com>
2023-03-09 15:35:27 -0700

    remove unnecessary

--- web/src/components/sidebar/index.tsx
+++ web/src/components/sidebar/index.tsx
@@ -1,11 +1,9 @@
 import { SearchBar } from "./searchBar";
 
-import { Space } from "@/types/interface";
 import { useContext } from "react";
 import { MapContext } from "@/context/map";
 
 export const Sidebar = () => {
-  const { spaces } = useContext(MapContext);
   return (
     <nav className="absolute top-0 h-full w-1/4 bg-white">
       <SearchBar />
