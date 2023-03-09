0c7b306b4c28ebfd29304c1da12b3fdcf033ae61
kokoaj <kokiebisu@icloud.com>
2023-03-09 15:14:25 -0700

    feat(web): set up input component (#532)

--- web/src/components/input/input-password/index.stories.tsx
+++ web/src/components/input/input-password/index.stories.tsx
@@ -4,7 +4,7 @@ import Component from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Component> = {
-  title: "Components/Avatar",
+  title: "Components/Input",
   component: Component,
   argTypes: {},
 };
@@ -13,6 +13,9 @@ export default meta;
 type Story = StoryObj<typeof Component>;
 
 // More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
-export const NoProfilePic: Story = {
-  args: {},
+export const Password: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Placeholder",
+  },
 };
