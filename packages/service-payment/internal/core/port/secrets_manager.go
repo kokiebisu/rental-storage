26bab1f177f931dc154cc465f387071479df8ba8
kokoaj <kokiebisu@icloud.com>
2023-03-13 23:21:47 -0600

    chore (#546)

--- packages/service-payment/internal/core/port/secrets_manager.go
+++
@@ -1,7 +0,0 @@
-package port
-
-import "github.com/aws/aws-sdk-go/service/ssm"
-
-type SecretsManager interface {
-	GetParam() (*ssm.GetParameterOutput, error)
-}
--- packages/service-payment/internal/core/port/service.go
+++
@@ -1,9 +0,0 @@
-package port
-
-type CustomerService interface {
-	CreateCustomer(userId string, firstName string, lastName string, emailAddress string) error
-}
-
-type PurchaseService interface {
-	MakePayment() error
-}
