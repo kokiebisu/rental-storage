872d0b89db6bb1f5d71e052cc3c9e3226bb45cb8
kokiebisu <kokiebisu@icloud.com>
2023-03-15 21:14:07 -0600

    initial

--- packages/composition/src/adapter/usecase/findSpaces.ts
+++ packages/composition/src/adapter/usecase/findSpaces.ts
@@ -1,5 +1,8 @@
 import { RestAPIClient } from "../../client";
-import { SpaceResourceURLBuilder } from "../../resource";
+import {
+  BookingResourceURLBuilder,
+  SpaceResourceURLBuilder,
+} from "../../resource";
 
 interface FindSpacesCommandConstructor {
   userId?: string;
@@ -40,14 +43,49 @@ export class FindSpacesUseCase {
     }
     const url = builder.findSpaces(param);
 
-    const response = await client.get<{
+    const spaceResponse = await client.get<{
       spaces: (Omit<ISpace, "id"> & { uid: string })[];
     }>(url);
-    return response.data.spaces.map(
+
+    const bookingResourceBuilder = new BookingResourceURLBuilder();
+
+    const pendingBookings: IBooking[] = [];
+
+    for (const space of spaceResponse.data.spaces) {
+      const bookingPendingResponse = await client.get<{
+        bookings: (Omit<IBooking, "id"> & { uid: string })[];
+      }>(bookingResourceBuilder.findPendingBookings({ spaceId: space.uid }));
+      for (const booking of bookingPendingResponse.data.bookings) {
+        pendingBookings.push({
+          ...booking,
+          id: booking.uid,
+        });
+      }
+    }
+
+    const approvedBookings: IBooking[] = [];
+
+    for (const space of spaceResponse.data.spaces) {
+      const bookingApprovedResponse = await client.get<{
+        bookings: (Omit<IBooking, "id"> & { uid: string })[];
+      }>(bookingResourceBuilder.findApprovedBookings({ spaceId: space.uid }));
+      for (const booking of bookingApprovedResponse.data.bookings) {
+        approvedBookings.push({
+          ...booking,
+          id: booking.uid,
+        });
+      }
+    }
+
+    return spaceResponse.data.spaces.map(
       (space: Omit<ISpace, "id"> & { uid: string }) => {
         return {
           id: space.uid,
           ...space,
+          bookings: {
+            pending: pendingBookings,
+            approved: approvedBookings,
+          },
         };
       }
     );
