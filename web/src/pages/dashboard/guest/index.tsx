9ecc4da4e0ee5d889e7a6aa9b92d7cf1edab7df3
kokoaj <kokiebisu@icloud.com>
2023-03-15 21:17:16 -0600

    feat(web): display all bookings for user guest on guest dashboard (#571)

--- web/src/pages/dashboard/guest/index.tsx
+++ web/src/pages/dashboard/guest/index.tsx
@@ -1,11 +1,44 @@
+import { useQuery } from "@apollo/client";
+
+import { awsLambdaClient } from "@/clients";
+import { MY_BOOKINGS_QUERY } from "@/graphql/queries";
 import { DefaultLayout } from "@/layout";
+import { Booking } from "@/types/interface";
 
 export default function Dashboard() {
+  const { data, loading, error } = useQuery(MY_BOOKINGS_QUERY, {
+    client: awsLambdaClient,
+  });
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  if (error) {
+    return <div>error...</div>;
+  }
+
+  console.log("DATA: ", data);
   return (
     <DefaultLayout>
       <div className="w-full h-full">
         <div>
-          <p className="text-center">My bookings (pending, confirmed)</p>
+          <div>Pending Bookings</div>
+          <div>
+            {data.profile.bookings.pending.length > 0
+              ? data.profile.bookings.pending.map((booking: Booking) => {
+                  return <div key={booking.id}>{JSON.stringify(booking)}</div>;
+                })
+              : null}
+          </div>
+        </div>
+        <div>
+          <div>Approved Bookings</div>
+          <div>
+            {data.profile.bookings.approved.length > 0
+              ? data.profile.bookings.approved.map((booking: Booking) => {
+                  return <div key={booking.id}>{JSON.stringify(booking)}</div>;
+                })
+              : null}
+          </div>
         </div>
       </div>
     </DefaultLayout>
