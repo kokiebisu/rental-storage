1b0f7ad836b05d0483c5ed1e66ef77ca1737a800
kokoaj <kokiebisu@icloud.com>
2023-03-23 23:49:02 -0600

    chore(authentication) (#588)

--- packages/service-authentication/internal/adapter/store/token.go
+++ packages/service-authentication/internal/adapter/store/token.go
@@ -17,12 +17,14 @@ var (
 	RefreshTokenPrefix = "refresh_token_"
 )
 
+// NewTokenStore creates a new token store
 func NewTokenStore(client *redis.Client) *TokenStore {
 	return &TokenStore{
 		client,
 	}
 }
 
+// SetAccessToken stores the access token for the user
 func (r *TokenStore) SetAccessToken(userID, token string, expires time.Duration) error {
 	key := AccessTokenPrefix + userID
 
@@ -40,18 +42,21 @@ func (r *TokenStore) GetAccessToken(userID string) (string, error) {
 	return token, nil
 }
 
+// DeleteAccessToken deletes the access token for the user
 func (r *TokenStore) DeleteAccessToken(userID string) error {
 	key := AccessTokenPrefix + userID
 
 	return r.client.Del(context.Background(), key).Err()
 }
 
+// SetRefreshToken stores the refresh token for the user
 func (r *TokenStore) SetRefreshToken(userID, token string, expires time.Duration) error {
 	key := RefreshTokenPrefix + userID
 
 	return r.client.Set(context.Background(), key, token, expires).Err()
 }
 
+// GetRefreshToken gets the refresh token for the user
 func (r *TokenStore) GetRefreshToken(userID string) (string, error) {
 	key := RefreshTokenPrefix + userID
 
@@ -63,12 +68,14 @@ func (r *TokenStore) GetRefreshToken(userID string) (string, error) {
 	return token, nil
 }
 
+// DeleteRefreshToken deletes the refresh token for the user
 func (r *TokenStore) DeleteRefreshToken(userID string) error {
 	key := RefreshTokenPrefix + userID
 
 	return r.client.Del(context.Background(), key).Err()
 }
 
+// VerifyAccessToken verifies the access token for the user
 func (r *TokenStore) VerifyAccessToken(token string, userID string) error {
 	key := AccessTokenPrefix + userID
 
@@ -84,6 +91,7 @@ func (r *TokenStore) VerifyAccessToken(token string, userID string) error {
 	return nil
 }
 
+// VerifyRefreshToken verifies the refresh token for the user
 func (r *TokenStore) VerifyRefreshToken(token string, userID string) error {
 	key := RefreshTokenPrefix + userID
 
