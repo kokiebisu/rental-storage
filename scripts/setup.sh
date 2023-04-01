e2cc85ac29e1af7135d0098ce51d5315fe05d2e4
kokiebisu <kokiebisu@icloud.com>
2023-03-31 17:35:31 -0700

    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,36 +6,44 @@ set -e
 
 export AWS_PROFILE=rental-storage
 
-function check_docker_running() {
-    # if docker is not running exit
-    if ! docker ps > /dev/null 2>&1; then
-        echo "Docker is not running. Please start docker and try again."
-        exit 1
+function check_called_path() {
+    # check if the script is called from the root directory
+    if [ ! -f "scripts/setup.sh" ]; then
+        echo "Please run this script from the root directory of the project.";
+        exit 1;
     fi
 }
 
-function vscode_prepare() {
+function check_docker_running() {
+    if ! docker ps > /dev/null 2>&1; then
+        echo "Docker is not running. Please start docker and try again.";
+        exit 1;
+    fi
+}
+
+function setup_vscode() {
     echo "Setting up vscode configurations...";
     (mkdir -p .vscode && cp config/settings.json .vscode/settings.json);
 }
 
-function install_dependencies() {
+function setup_deps() {
     echo "Installing dependencies...";
-    (bash ./scripts/setup-deps.sh)
+    (bash ./scripts/setup-deps.sh);
 }
 
 function setup_terraform() {
     echo "Setting up terraform configurations...";
-    (bash ./scripts/setup-terraform.sh $1)
+    (bash ./scripts/setup-terraform.sh $1);
 }
 
 function setup_serverless() {
     echo "Setting up serverless configurations...";
-    (bash ./scripts/setup-service.sh)
+    (bash ./scripts/setup-service.sh);
 }
 
+check_called_path
 check_docker_running
-vscode_prepare
-install_dependencies
+setup_vscode
+setup_deps
 setup_terraform local
 setup_serverless
