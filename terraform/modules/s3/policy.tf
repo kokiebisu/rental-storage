102c1e479d18bf6e36a4b0f8aa1cf2e936e3c05d
kokiebisu <kokiebisu@icloud.com>
2023-05-17 07:02:54 -0700

    fix

--- terraform/modules/s3/policy.tf
+++ terraform/modules/s3/policy.tf
@@ -1,21 +1,21 @@
-# need to create iam 'terraform' user first
-resource "aws_s3_bucket_policy" "space_profile" {
-  bucket = aws_s3_bucket.space_profile.id
+# # need to create iam 'terraform' user first
+# resource "aws_s3_bucket_policy" "space_profile" {
+#   bucket = aws_s3_bucket.space_profile.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-          Sid = "AllowAllS3Actions",
-          Effect = "Allow",
-          Action = [
-              "s3:GetObject",
-              "s3:PutObject",
-              "s3:PutObjectAcl"
-          ],
-          Principal = "*",
-          Resource = "${aws_s3_bucket.space_profile.arn}/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#           Sid = "AllowAllS3Actions",
+#           Effect = "Allow",
+#           Action = [
+#               "s3:GetObject",
+#               "s3:PutObject",
+#               "s3:PutObjectAcl"
+#           ],
+#           Principal = "*",
+#           Resource = "${aws_s3_bucket.space_profile.arn}/*"
+#       }
+#     ]
+#   })
+# }
--- terraform/modules/s3/space_profile.tf
+++ terraform/modules/s3/space_profile.tf
@@ -2,21 +2,21 @@ resource "aws_s3_bucket" "space_profile" {
   bucket = "${var.environment}-${var.account_id}-space-profile"
 }
 
-resource "aws_s3_bucket_public_access_block" "space_profile" {
-  bucket = aws_s3_bucket.space_profile.id
+# resource "aws_s3_bucket_public_access_block" "space_profile" {
+#   bucket = aws_s3_bucket.space_profile.id
 
-  block_public_acls       = true
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = false
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-resource "aws_s3_bucket_cors_configuration" "space_profile" {
-  bucket = aws_s3_bucket.space_profile.id
+# resource "aws_s3_bucket_cors_configuration" "space_profile" {
+#   bucket = aws_s3_bucket.space_profile.id
 
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "PUT", "POST", "HEAD"]
-    allowed_origins = ["*"]
-  }
-}
+#   cors_rule {
+#     allowed_headers = ["*"]
+#     allowed_methods = ["GET", "PUT", "POST", "HEAD"]
+#     allowed_origins = ["*"]
+#   }
+# }
