db00e080f9729a86b1d23743e8dcbbd0aca5488b
kokoaj <kokiebisu@icloud.com>
2023-05-20 18:17:25 -0700

    feat(web): s3 image should be publicly readable (#708)

--- terraform/modules/s3/space_profile.tf
+++ terraform/modules/s3/space_profile.tf
@@ -13,3 +13,28 @@ resource "aws_s3_bucket_cors_configuration" "space_profile" {
     allowed_origins = ["*"]
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.space_profile.id
+
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "space_profile" {
+  bucket = aws_s3_bucket.space_profile.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.space_profile.arn}/*"
+      }
+    ]
+  })
+}
